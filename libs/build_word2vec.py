403e52140c364a574d2ecef95c3df339b5598d48
KenDooley <kenneth.dooley@gmail.com>
2015-03-18 17:38:09 -0700

    clean up

--- libs/build_word2vec.py
+++ libs/build_word2vec.py
@@ -5,14 +5,12 @@ create our word2vec model and, from that, calculate our document vectors:
     sentences in the corpus to an intermediate file which could then be
     streamed from into the model.
 2) Calculate Doc2Vec matrix -- For each document in the corpus, a document
-    vector is calculated from the word2vec vectors of the individual words
-    which appear in that document.  The final matrix in then pickeled to
-    disk.
+    vector is calculated from the word2vec vectors of the individual terms
+    which appear in that document.  The final matrix in then pickeled
+    to disk. This matrix representation was ultimately chosen to be my final
+    model.
 3) Create final document vectors -- In this step we append the LSI matrix
     calculated in stage one to the Doc2Vec matrix calculated in step 2 above.
-    This yields a unique semantic 'thumbprint' for each restaurant from
-    which we can calculate cosine similarities.  This matrix is then
-    pickled to disk along with its resulting similarity matrix.
 """
 
 from gensim import corpora, models, similarities, matutils
@@ -26,6 +24,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
 
 TFIDF_MODEL = models.TfidfModel.load('../data/model.tfidf')
+
 
 class SentencesFromFile(object):
     """
@@ -103,9 +102,10 @@ def doc_matrix(corpus, w2v_model, dictionary):
 def doc2vec(corpus_doc, w2v_model, dictionary):
     """
     Function for calculating a document vector for a single document in our
-    corpus.  This is done by calculating the average word vector over all words
-    in the document.  Words which appear in our corpus < 5 times are not
-    considered.
+    corpus.  This is done by calculating the weighted average word vector
+    over all words in the document.  The weightings are determined by each
+    word's TFIDF weight.  Words which appear in our corpus < 5 times are
+    not considered.
 
     INPUT: corpus_doc - a single document from a gensim corpus object
            w2v_model - a trained gensim word2vec model
@@ -120,20 +120,19 @@ def doc2vec(corpus_doc, w2v_model, dictionary):
             a = w2v_model[dictionary[word[0]]]
             tot_wgt += word[1]
         except:
-            pass
+            logging.warning("%s not in Word2Vec" % word[0])
 
-    counter = 0
     output = np.zeros(300)
     for word in TFIDF_MODEL[corpus_doc]:
         try:
             output += w2v_model[dictionary[word[0]]] * (word[1] / tot_wgt)
-            counter += 1
         except:
             logging.warning("%s not in dictionary" % word[0])
-    return output# / float(counter)
+    return output
 
 
-def save_lsi_w2v_matrix(corpus, tfidf_model, lsi_model):
+def save_lsi_w2v_matrix(corpus, tfidf_model, lsi_model,
+                        filename='../data/big_matrix_wgt.index'):
     """
     Function which concatenates the LSI matrix created in stage 1 to the
     Doc2Vec matrix created by 'doc_matrix'.  The resulting matrix is pickled
@@ -153,20 +152,16 @@ def save_lsi_w2v_matrix(corpus, tfidf_model, lsi_model):
     file1 = open('../data/big_matrix_wgt.obj', 'w')
     pickle.dump(big_matrix.T, file1)
     file1.close()
-    index.save('../data/big_matrix_wgt.index')
-
-
-
-
+    index.save(filename)
 
 if __name__ == '__main__':
 
-    # gen_sentence_file(phrase_file='../data/bigrams')
-    # build_word2vec()
+    gen_sentence_file(phrase_file='../data/bigrams')
+    build_word2vec()
     word2vec = models.Word2Vec.load('../data/mymodel')
     dictionary = corpora.Dictionary.load('../data/restaurants.dict')
     corpus = corpora.MmCorpus('../data/corpus.mm')
     tfidf_model = models.TfidfModel.load('../data/model.tfidf')
     lsi_model = models.LsiModel.load('../data/model.lsi')
-    # save_doc_matrix(corpus, word2vec, dictionary)
+    save_doc_matrix(corpus, word2vec, dictionary)
     save_lsi_w2v_matrix(corpus, tfidf_model, lsi_model)
