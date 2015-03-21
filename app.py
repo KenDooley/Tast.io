022f01a0d8170bdf853ed1233e1a0bd95a692921
KenDooley <kenneth.dooley@gmail.com>
2015-03-20 20:22:55 -0700

    adding full US search

--- app.py
+++ app.py
@@ -4,21 +4,24 @@
 from flask import Flask, url_for, redirect
 from flask import render_template
 from flask import request
-from gensim import similarities
+from gensim import similarities, models, corpora
 from geopy.distance import vincenty
+import libs.scraper as scrape
 import libs.yelp.search as yelp
 import numpy as np
 import json
 import pickle
 
-KEY = "XXXXX"
-SECRET_KEY = "XXXXX"
-TOKEN = "XXXXXX"
-SECRET_TOKEN = "XXXXX"
-
-app = Flask(__name__)
+KEY = "YtYo3-wypceY7A63JaJPAw"
+SECRET_KEY = "xLX6MySk_gFK3cGI_FzwethvqDU"
+TOKEN = "BdQOXSrzwX6SO6vuAuzHLCUNOmGEMxCj"
+SECRET_TOKEN = "1Xcrnf4VVGWQYk-bOWGacaipNtc"
 
 INDEX = similarities.MatrixSimilarity.load('data/w2v_matrix_wgt.index')
+BIGRAMS = models.Phrases.load('data/bigrams')
+DICTIONARY = corpora.Dictionary.load('data/restaurants.dict')
+TFIDF = models.TfidfModel.load('data/model.tfidf')
+WORD2VEC = models.Word2Vec.load('data/mymodel')
 
 with open('data/idx_name.obj', 'r') as f:
     IDX_NAME = pickle.load(f)
@@ -29,6 +32,7 @@ with open('data/name_idx.obj', 'r') as g:
 with open('data/w2v_matrix_wgt.obj', 'r') as h:
     BIG_MATRIX = pickle.load(h)
 
+app = Flask(__name__)
 
 @app.route('/')
 def restaurant_query():
@@ -61,20 +65,18 @@ def get_similarities():
         target_distance = 2
 
     search_string = request.form['user_input'].lower()
+    print search_string
     target = request.form['state']
     name = '+'.join(search_string.split(',')[0].split()).encode('utf8')
 
     try:
-        state = search_string.split(',')[-2].upper().encode('utf8').strip()
+        temp = [item.strip().upper() for item in search_string.split(',')[-3:-1]]
+        temp[0] += ","
+        temp = ["+".join(item.split()) for item in temp]
+        city = "+".join(temp).encode('utf8')
     except:
         return redirect(url_for('restaurant_query'))
 
-    if state == 'CA':
-        city = "SAN+FRANCISCO"
-    elif state == 'NY':
-        city = "NEW+YORK,+NY"
-    else:
-        return redirect(url_for('restaurant_query'))
 
     params = {'location': city,
               'term': name,
@@ -84,8 +86,12 @@ def get_similarities():
 
     d = yelp.request(params, KEY, SECRET_KEY, TOKEN, SECRET_TOKEN)
 
-    if len(d['businesses']) < 2:
+    if len(d['businesses']) == 0:
+        print "Not in Yelp"
+        return redirect(url_for('restaurant_query'))
+    elif len(d['businesses']) < 2:
         biz = d['businesses'][0]['name'].lower().encode('utf8')
+        long_review = 0
     else:
         long_review = np.argmax([item['review_count'] for item
                                 in d['businesses']])
@@ -102,15 +108,19 @@ def get_similarities():
     biz = biz.replace('é', 'e')
 
     if " ".join(name.split('+'))[:2].lower() != biz[:2]:
+        print "Wrong restaurant"
         return redirect(url_for('restaurant_query'))
 
     try:
         ids = NAME_IDX[biz.decode('utf8')]
+        vec = BIG_MATRIX[ids]
     except:
-        print "not in database", len(biz)
-        return redirect(url_for('restaurant_query'))
+        vec = scrape.get_search_vector(d['businesses'][long_review], BIGRAMS, DICTIONARY, TFIDF, WORD2VEC)
+        if len(vec) == 0:
+            print "Could not find reviews"
+            return redirect(url_for('restaurant_query'))
 
-    vec = BIG_MATRIX[ids]
+    
     sims = INDEX[vec]
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
 
@@ -119,7 +129,7 @@ def get_similarities():
     len_targs = len(biz)
     if local_search == 0:
         for idx, sim in enumerate(sims):
-            if IDX_NAME[sim[0]]['location'] == target and
+            if IDX_NAME[sim[0]]['location'] == target and \
             IDX_NAME[sim[0]]['name'].lower()[:len_targs].encode('utf8') != biz:
                 output[counter] = IDX_NAME[sim[0]]
                 output[counter]["sim_score"] = "{0:.3f}".format(round(sim[1], 3))
@@ -133,8 +143,8 @@ def get_similarities():
                                 IDX_NAME[sim[0]]['coordinate']['longitude'])
             except:
                 continue
-            if IDX_NAME[sim[0]]['location'] == target and
-            IDX_NAME[sim[0]]['name'].lower()[:len_targs].encode('utf8') != biz
+            if IDX_NAME[sim[0]]['location'] == target and \
+            IDX_NAME[sim[0]]['name'].lower()[:len_targs].encode('utf8') != biz \
             and vincenty(current_location, geo_location).miles < target_distance:
                 output[counter] = IDX_NAME[sim[0]]
                 output[counter]["sim_score"] = "{0:.3f}".format(round(sim[1], 3))
---
+++ libs/scraper.py
@@ -0,0 +1,172 @@
+"""
+This script is used for scraping the reviews data for a single target
+business who's most similar 'neighbors' we are searching for.
+
+"""
+
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+import numpy as np 
+import requests
+import logging
+import time
+
+MIN_REVIEWS = 20
+UNICODE_PUNC = u'!"#%\'()*+,-./:;<=>?@[\]^_`{|}~'
+TABLE = dict((ord(char), u' ') for char in UNICODE_PUNC)
+STEMMER = PorterStemmer()
+STOPWORDS = stopwords.words('english')
+
+
+# Proxy login information to avoid IP blocks
+# AUTH = requests.auth.HTTPProxyAuth('XXXX', 'XXXX')
+# PROXIES = {'http': 'http://XXXXXX'}
+
+def tokenize(doc):
+        """
+        Tokenizes the input sting and returns the tokens in a list.
+
+        INPUT: doc -- a string to be tokenized
+        OUTPUT: a list of stemmed words (minus stopwords and punctuation) in
+               the doc.
+        """
+        global STEMMER, STOPWORDS
+        no_punc = remov_punc(doc)
+        tokens = word_tokenize(no_punc.lower())
+        lemmas_no_stop = [STEMMER.stem(word) for word in tokens
+                          if word not in STOPWORDS]
+        return lemmas_no_stop
+
+def remov_punc(text):
+        """
+        Removes unicode punctuation from 'text' and replaces with a space.
+        """
+        global TABLE
+        return text.translate(TABLE)
+
+def doc2vec(tfidf_doc, w2v_model, dictionary):
+    """
+    Function for calculating a document vector for a single search document.
+    This is done by calculating the weighted average word vector
+    over all words in the document.  The weightings are determined by each
+    word's TFIDF weight.  Words which appear in our corpus < 5 times are
+    not considered.
+
+    INPUT: corpus_doc - a single document from a gensim corpus object
+           w2v_model - a trained gensim word2vec model
+           dictionary - a gensim dictionary object
+
+    OUTPUT: a numpy array of dimension equal to the dimensionality of our
+           trained Word2Vec model (300 in this case).
+    """
+    if len(tfidf_doc) == 0:
+        return []
+
+    tot_wgt = 0.0
+    for word in tfidf_doc:
+        try:
+            a = w2v_model[dictionary[word[0]]]
+            tot_wgt += word[1]
+        except:
+            logging.warning("%s not in Word2Vec" % word[0])
+
+    output = np.zeros(300)
+    for word in tfidf_doc:
+        try:
+            output += w2v_model[dictionary[word[0]]] * (word[1] / tot_wgt)
+        except:
+            logging.warning("%s not in dictionary" % word[0])
+    return output
+
+def get_npages(url):
+    """
+    This function returns the number of pages of reviews on Yelp for the
+    restaurant who's main Yelp page is specified in 'url'.
+    """
+
+    text = try_requests(url)
+    if len(text) > 0:
+        soup = BeautifulSoup(text, "html.parser")
+        pages = soup.find('div', class_="page-of-pages")
+        if pages:
+            pages = pages.text.split()[-1]
+        else:
+            pages = "1"
+        return int(pages)
+    return 0
+
+
+def try_requests(url):
+    """
+    This function attempts to access the html of the target webpage in 'url'.
+    If the attempt is unsuccessful, the function tries again three more times
+    before giving up and throwing a warning.
+
+    INPUT: url -- url address of target webpage to be loaded.
+    OUTPUT: The html text of the target webpage
+    """
+
+    counter = 0
+    try:
+        r = requests.get(url)
+    except:
+        logging.warning('Could not access link %s' % url)
+        return ""
+
+    if r.status_code == 200:
+        return r.text
+    else:
+        while counter < 3:
+            counter += 1
+            time.sleep(5)
+            r = requests.get(url)
+            if r.status_code == 200:
+                return r.text
+        logging.warning('Could not access link %s' % url)
+        logging.warning("%d" % r.status_code)
+        return ""
+
+
+def get_search_vector(busi, phrase_file, dict_file, tfidf_file, word2vec_file):
+    """
+    This function scrapes the review texts for a given search restaurant.
+
+    INPUT: busi - a Yelp search output JSON for a search target.
+    OUTPUT: The doc2vec representation of the restaurant (array)
+
+    """
+
+    global MIN_REVIEWS
+    logging.warning('getting reviews for ' + busi['name'])
+
+    rev = []
+    output = u""
+
+    if (busi.get('review_count', 0) > MIN_REVIEWS):
+
+        npages = get_npages(busi['url'])
+
+        pagen = min(13, npages)
+        for page in xrange(pagen):
+
+            index = page * 40
+            if page == 0:
+                url = '%s' % busi['url']
+            else:
+                url = '%s?start=%d' % (busi['url'], index)
+            text = try_requests(url)
+
+            if len(text) > 0:
+                soup = BeautifulSoup(text, 'html.parser')
+                bucket = soup.find_all('p', itemprop='description')
+                rev.extend([item.text.replace(u'\xa0', u' ') for item in bucket])
+
+        
+        output = u" ".join(rev)
+    output = tfidf_file[dict_file.doc2bow(phrase_file[tokenize(output)])]
+    return doc2vec(output, word2vec_file, dict_file)
+
+
+
